bd0786619c61d06d0fe99883fc1f33ede05812f0
strixqlater <strixqlater@gmail.com>
2020-11-12 12:51:52 +0900

    GoodsController 수정

--- MyShop/src/main/java/kr/daoko/controller/GoodsController.java
+++ MyShop/src/main/java/kr/daoko/controller/GoodsController.java
@@ -1,6 +1,6 @@
 package kr.daoko.controller;
 
-import java.util.List;
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.daoko.dto.GoodsDTO;
 import kr.daoko.service.GoodsService;
@@ -18,19 +17,23 @@ import kr.daoko.service.GoodsService;
 @RequestMapping("/goods/*")
 public class GoodsController {	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Inject
 	GoodsService g_service;
+	
 	@GetMapping("/manage")
 	public String getManage() {
 		logger.info("getGoodsManage()");
 		return "goods/manage";
 	}
-	//상품 등록 창
 	
+	//상품 등록 창
 	@GetMapping("/addwindow")
 	public String getAddWindow(Model model) throws Exception {
 		logger.info("getGoodsaddWindow()");
 		return "goods/add";
 	}
+	
 	//상품 등록
 	@PostMapping("/addwindow")
 	public String postGoodsAdd(GoodsDTO dto) throws Exception {
